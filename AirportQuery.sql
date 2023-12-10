866c572e4fb734c45cf4c312a6e187796daa9db8
rokoperisic <roko.perisic@gmail.com>
2023-12-10 19:18:44 +0100

    Capacity triggers

--- AirportQuery.sql
+++ AirportQuery.sql
@@ -100,13 +100,13 @@ create table Comments(
 	Comment varchar(100) 
 );
 
-ALTER TABLE Planes
-ADD CONSTRAINT CheckStatus CHECK (Status IN ('Na prodaji', 'Aktivan', 'Na popravku', 'Razmontiran'));
+alter table Planes
+add constraint CheckStatus check (Status in ('Na prodaji', 'Aktivan', 'Na popravku', 'Razmontiran'));
 
-ALTER TABLE Pilots
-ADD CONSTRAINT CheckPilotAge
-CHECK (
-    AGE(Birth) BETWEEN INTERVAL '20 years' AND INTERVAL '60 years'
+alter table Pilots
+add constraint CheckPilotAge
+check (
+    AGE(Birth) between INTERVAL '20 years' and INTERVAL '60 years'
 );
 
 alter table ticketsusers
@@ -118,3 +118,66 @@ add constraint CheckPilotsNumber check(PilotId <= 2);
 alter table FlightsStewardesses 
 add constraint CheckStewardessesNumber check(StewardessId <= 4);
 
+create function check_flight_capacity()
+return trigger as $$
+declare
+    plane_capacity_economy int;
+    plane_capacity_first_class int;
+    flight_capacity int;
+begin
+    select CapacityEconomy, CapacityFirstClass
+    into plane_capacity_economy, plane_capacity_first_class
+    from Planes
+    where PlaneId = new.PlaneId;
+
+    select new.CapacityEconomy + new.CapacityFirstClass
+    into flight_capacity;
+
+    if flight_capacity > plane_capacity_economy + plane_capacity_first_class then
+        raise exception 'Flight capacity exceeds plane capacity';
+    end if;
+
+    return new;
+end;
+$$ LANGUAGE plpgsql;
+
+create trigger enforce_flight_capacity
+before insert or update on Flights
+for each row execute function check_flight_capacity();
+
+alter table Seats
+add constraint CheckTicketClass Check(Class in ('A','B'))
+
+create function check_seat_number()
+returns trigger as $$
+declare
+    capacity_economy int;
+	capacity_first_class int;
+    seat_number int;
+begin
+    select CapacityEconomy
+    into capacity_economy
+	from Flights
+    where FlightId = new.FlightId;
+	
+	select CapacityFirstClass
+	into capacity_first_class
+    from Flights
+    where FlightId = new.FlightId;
+
+    select Number
+    into seat_number
+    from Seats
+    where SeatId = new.SeatId;
+
+    if seat_number > capacity_economy or seat_number > capacity_first_class then
+        raise exception 'Seat number exceeds the flight capacity';
+    end if;
+
+    return new;
+end;
+$$ language plpgsql;
+
+create trigger enforce_seat_number
+before insert or update on Tickets
+for each row execute function check_seat_number();
